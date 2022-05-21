a5a96b2aabbb6919e1a9027c6b12a6f421dfd117
cristianyil <83972738+cristianyil@users.noreply.github.com>
2022-05-21 15:58:52 +0200

    pass to Function

---
+++ script.js
@@ -0,0 +1,164 @@
+// Funcion que hace todas las validaciones
+function validateForm(event) {
+    // Creo un diccionario clave valor para facilitar la lectura de codigo, lo vi aqui https://www.w3schools.com/js/js_objects.asp
+    let formData = {
+        name: document.forms["formulario"]["nombre"].value,
+        telefono: document.forms["formulario"]["tel"].value,
+        correo: document.forms["formulario"]["correo"].value,
+        password1: document.forms["formulario"]["pass1"].value,
+        password2: document.forms["formulario"]["pass2"].value,
+        fnac: document.forms["formulario"]["naci"].valueAsDate,
+        country: document.forms["formulario"]["pais"].value,
+        city: document.forms["formulario"]["ciudad"].value,
+    };
+
+    // Llamamos a las funciones que he creado para que compruebe si es validos los datos, si trae null no hay ningun fallo
+    const nameCheck = checkName(formData.name);
+    const phoneCheck = checkPhone(formData.telefono);
+    const mailCheck = checkMail(formData.correo);
+    const dateCheck = checkDate(formData.fnac);
+    const countryCheck = checkCountry(formData.country);
+    const cityCheck = checkCity(formData.city);
+    const passwordCheck = checkPasswordValidation(formData.password1, formData.password2);
+
+    // Veremos si hay alguno que haya dado error, es decir que no sea nulo, lanzamos mensaje y no enviamos el formulario
+    if (nameCheck != null) {
+        document.getElementById("errorD").innerHTML = nameCheck;
+        event.preventDefault();
+    } else if (phoneCheck != null) {
+        document.getElementById("errorD").innerHTML = phoneCheck;
+        event.preventDefault();
+    } else if (mailCheck != null) {
+        document.getElementById("errorD").innerHTML = mailCheck;
+        event.preventDefault();
+    } else if (passwordCheck != null) {
+        document.getElementById("errorD").innerHTML = passwordCheck;
+        event.preventDefault();
+    } else if (countryCheck != null) {
+        document.getElementById("errorD").innerHTML = countryCheck;
+        event.preventDefault();
+    } else if (cityCheck != null) {
+        document.getElementById("errorD").innerHTML = cityCheck;
+        event.preventDefault();
+    } else if (dateCheck != null) {
+        document.getElementById("errorD").innerHTML = dateCheck;
+        event.preventDefault();
+    }
+}
+
+function checkPasswordValidation(password, passwordCopy) {
+    const isWhitespace = /^(?=.*\s)/;
+    if (password.match(isWhitespace)) {
+        return "Password must not contain Whitespaces.";
+    }
+
+    const isContainsUppercase = /^(?=.*[A-Z])/;
+    if (!password.match(isContainsUppercase)) {
+        return "Password must have at least one Uppercase Character.";
+    }
+
+
+    const isContainsLowercase = /^(?=.*[a-z])/;
+    if (!password.match(isContainsLowercase)) {
+        return "Password must have at least one Lowercase Character.";
+    }
+
+
+    const isContainsNumber = /^(?=.*[0-9])/;
+    if (!password.match(isContainsNumber)) {
+        return "Password must contain at least one Digit.";
+    }
+
+
+    const isContainsSymbol = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
+    if (!password.match(isContainsSymbol)) {
+        return "Password must contain at least one Special Symbol.";
+    }
+
+
+    const isValidLength = /^.{4,15}$/;
+    if (!password.match(isValidLength)) {
+        return "Password must be 10-16 Characters Long.";
+    }
+
+    if (password !== passwordCopy) {
+        return "las contraseñas no coinciden";
+    }
+
+}
+
+function checkName(name) {
+
+
+    if (name.length <= 2) {
+        return "Nombre invalido 2 caracteres minimo"
+    }
+
+    if (name.length > 15) {
+        return "Nombre invalido 15 caracteres maximo"
+    }
+
+    if (!name.match("^[A-Za-z]+$")) {
+        return "Nombre invalido, introduzca solo letras sin espacios "
+    }
+}
+
+function checkPhone(phone) {
+    let especial = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/
+
+    if (phone.length !== 9) {
+        return "introduce un numero valido";
+    }
+    if (phone.match("^[A-Za-z]+$") || phone.match(especial)) {
+        return "introduce un numero valido";
+    }
+}
+
+function checkMail(mail) {
+    if (!mail.match(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i)) {
+        return "correo invalido";
+    }
+}
+
+function checkDate(fnac) {
+    const date = new Date(Date.now());
+    if (fnac === "" || fnac == null) {
+        return  "Porfavor introduzca la edad";
+    }
+    if (fnac > date) {
+        return "la fecha no puede ser futura";
+    }
+    if ((date.getFullYear() - fnac.getFullYear()) <= 18) {
+        if (date.getFullYear() - fnac.getFullYear() < 18) {
+            return "No cumple la mayoria de edad";
+        } else if (fnac.getMonth() <= date.getMonth()) {
+            if (fnac.getMonth() === date.getMonth()) {
+                if (fnac.getDate() > date.getDate()) {
+                    return "No cumple la mayoria de edad";
+                }
+            }
+        } else {
+            return "No cumple la mayoria de edad";
+        }
+    } else if (date.getFullYear() - fnac.getFullYear() > 66) {
+        return "No puede tener mas de 65 años";
+    } else if (date.getFullYear() - fnac.getFullYear() === 66) {
+        if (fnac.getMonth() >= date.getMonth()) {
+            if (fnac.getMonth() === date.getMonth()) {
+                if (fnac.getDate() <= date.getDate()) {
+                    return "No puede tener mas de 65 años";
+                }
+            }
+        } else {
+            return "No puede tener mas de 65 años";
+        }
+    }
+}
+
+function checkCountry(country){
+    return country === "" ? "pon un pais" : undefined
+}
+
+function checkCity(city){
+    return city === "" ? "pon una ciudad" : undefined
+}
---
+++ unitTest.js
@@ -0,0 +1,59 @@
+console.log("###### Pruebas unitarias ######")
+console.log("undefined significa que no da error")
+
+console.log("")
+console.log("---- Nombre ----")
+console.log("cristian: " + checkName("cristian"))
+console.log("cr: " + checkName("cr"))
+console.log("cristiannnnnnnnnnnnnnnnnnnn: " + checkName("cristiannnnnnnnnnnnnnnnnnnn"))
+console.log(".cristian: " + checkName(".cristian"))
+console.log("131231123: " + checkName("131231123"))
+console.log("cristian gil: " + checkName("cristian gil"))
+
+console.log("")
+console.log("---- Telefono ----")
+console.log("633510114: " + checkPhone("633510114"))
+console.log("633510114.: " + checkPhone("633510114."))
+console.log("aaaaaaaaa: " + checkPhone("aaaaaaaaa"))
+
+console.log("")
+console.log("---- Correo ----")
+console.log("cristian@gmail.com: " + checkMail("cristian@gmail.com"))
+console.log("cristiangmail.com: " + checkMail("cristiangmail.com"))
+console.log("cristian@gmail.c: " + checkMail("cristian@gmail.c"))
+
+
+console.log("")
+console.log("---- Password ----")
+console.log("123456789Cr? | 123456789Cr?: " + checkPasswordValidation("123456789Cr?","123456789Cr?"))
+console.log("123456789Cr? | pollo: " + checkPasswordValidation("123456789Cr?","pollo"))
+console.log("123456789Cr? | : " + checkPasswordValidation("123456789Cr?",""))
+console.log("123456789Cr | 123456789Cr: " + checkPasswordValidation("123456789Cr","123456789Cr"))
+
+console.log("")
+console.log("---- Password ----")
+console.log("123456789Cr? | 123456789Cr?: " + checkPasswordValidation("123456789Cr?","123456789Cr?"))
+console.log("123456789Cr? | pollo: " + checkMail("cristiangmail.com"))
+console.log("cristian@gmail.c: " + checkMail("cristian@gmail.c"))
+
+console.log("")
+console.log("---- Country ----")
+console.log("vacio: " + checkCountry(""))
+console.log("relleno: " + checkCountry("españa"))
+
+console.log("")
+console.log("---- City ----")
+console.log("vacio: " + checkCity(""))
+console.log("relleno: " + checkCity("la linea"))
+
+console.log("")
+console.log("---- Date ----")
+
+let dateOld = new Date("1956-05-22");
+dateOld.setDate(new Date().getDate()+1)
+
+console.log("1997-11-10: " + checkDate(new Date("1997-11-10")))
+console.log("2020-11-10: " + checkDate(new Date("2020-11-10")))
+console.log("1956-05-21: " + checkDate(new Date("1956-05-21")))
+console.log("1956-05-22: " + checkDate(dateOld))
+console.log("vacio: " + checkDate())
